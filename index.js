8f3835b15430ec226c84c7f6e9881633ca21b4ae
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 21:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  for(let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      array.slice()
+    }
+  }
+  return min
 }
 
 function insertionSort(array){
